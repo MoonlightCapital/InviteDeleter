023928351b780d28ddd8a1e4796d372cdf6e2298
MoonlightCapital <supertoad65@yahoo.com>
2018-12-03 19:45:12 +0100

    If the server owner is blacklisted, bot will automatically leave the server
    https://github.com/MoonlightCapital/InviteDeleter/projects/1#card-15400179
    
    Note: needs a fix in a certain point...

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,12 @@
 const {logHook} = require('../includes/logging')
 
-module.exports = (client, guild) => {
+module.exports = async (client, guild) => {
   logHook.send(`:arrow_right: Added into a server: **${client.utils.escapeMarkdown(guild.name)}** (\`${guild.id}\`)`)
+
+  const ownerData = await client.db.forceUser(guild.ownerID)
+
+  if(ownerData.powerlevel < 0) {
+    logHook.send(`:passport_control: ${guild.owner.user.tag} (\`${guild.ownerID}\`) is blacklisted, and tried to add me to a server they own`)
+    return guild.leave().catch(console.error)
+  }
 }
