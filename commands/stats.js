1b4e54aadd4f51d15d4619d377538863b5c1f037
MoonlightCapital <supertoad65@yahoo.com>
2018-12-03 21:29:52 +0100

    Stats command
    https://github.com/MoonlightCapital/InviteDeleter/projects/1#card-15410249

---
+++ commands/stats.js
@@ -0,0 +1,35 @@
+const {RichEmbed} = require('discord.js')
+const ms = require('ms')
+const {version} = require('../package')
+
+exports.run = async (client, message) => {
+
+  const uptime = ms(client.uptime, {long:true})
+  const serverCount = client.guilds.size
+
+  const embed = new RichEmbed()
+  .setTitle(`Statistics for ${client.user.username}`)
+  .setThumbnail(client.user.displayAvatarURL)
+  .setColor(0x008080)
+
+  .addField('Uptime', uptime)
+  .addField('Server count', serverCount)
+  .addField('Version', version)
+
+  message.channel.send(embed)
+}
+
+exports.help = {
+  name: 'stats',
+  info: 'Shows some bot statisics',
+  usage: '',
+  unlisted: false,
+}
+
+exports.config = {
+  guildOnly: false,
+  ownerOnly: false,
+  aliases: [],
+  minLevel: 0,
+  cooldown: 5,
+}
