4fdc26bfd4c3333b8f5b847323b4099acbb6787a
MoonlightCapital <supertoad65@yahoo.com>
2019-01-22 21:29:03 +0100

    Fix update

--- commands/update.js
+++ commands/update.js
@@ -1,26 +1,14 @@
 const {greentick, redtick} = require('../includes/emotes')
 const {logHook} = require('../includes/logging')
-const cp = require('child_process')
 
-function update(branch) {
-  return new Promise(function(resolve, reject) {
-    resolve(cp.exec(`git fetch origin && git reset --hard origin/${branch}`))
-  })
+const {execSync} = require('child_process')
+
+exports.run = async (client, message) => {
+  execSync('git fetch origin && git reset --hard origin/event')
+
+  message.channel.send(`${greentick} Bot updated successfully`)
 }
 
-exports.run = async (client, message, args) => {
-
-  const msg = await message.channel.send(`${greentick} Update started...`)
-
-  update(args[0]).then(() => {
-    msg.edit(`${greentick} Bot updated successfully. Remember to restart in order to make changes effective`)
-  }).catch((e) => {
-    console.error(e)
-    msg.edit(`${redtick} Something went wrong while updating`)
-  })
-
-  process.exit(0)
-}
 
 exports.help = {
   name: 'update',
