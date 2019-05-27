3c9a0e42b83fd381610c584898524ba63148d40c
MoonlightCapital <supertoad65@yahoo.com>
2019-05-27 22:09:41 +0200

    Bug fix on server bot remove

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,11 @@
 const event = require('../event-stuff/constants')
 
 module.exports = async (client, member) => {
+  if(message.author.bot) return
+
   const data = await client.db.getUser(member.id)
+
+  if(!data) return
 
   if(member.guild.id === event.server && data.powerlevel === 1)
     await client.db.updateUser({powerlevel: 0}, member.id)
