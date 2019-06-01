493e2094cb74e66598f03f8e8f981e97bd9f59eb
MoonlightCapital <supertoad65@yahoo.com>
2019-06-01 22:55:58 +0200

    gban spambots based on links sent

--- events/message.js
+++ events/message.js
@@ -2,14 +2,52 @@ const {redtick} = require('../includes/emotes')
 const {logHook, commandHook, errorHook, formatCommand} = require('../includes/logging')
 const {RichEmbed} = require('discord.js')
 
+const fetch = require('node-fetch')
+
+const url = 'https://gist.githubusercontent.com/MoonlightCapital/7baa23f180183ebe8bfe241b818a00ca/raw/phrase-blacklist.txt'
+
+let spamlist = []
+
+setInterval( async function() {
+  const body = await fetch(url).then(r=>r.text())
+  spamlist = body
+  .split('\n')
+  .filter(e=>!e.startsWith('##')) // Comments
+  .filter(e=> e !== '')
+  .map(e=>e.toLowerCase())
+  console.log(body, '\n', spamlist)
+}
+, 60000)
+
+const maxAge = 259200000 // 72 hours
+
 const cooldownCache = new Set()
 
 module.exports = async (client, message) => {
 
+  if(message.author.bot) return
+
+  if(message.guild && spamlist.some(e=>message.content.toLowerCase().includes(e)) && message.guild.roles.some(r=>r.name === '+enable-experimental-blacklist')) {
+
+    if((Date.now() - message.author.createdTimestamp) < maxAge) {
+      if(message.member.bannable) message.guild.ban(message.member.id, {reason: 'Automatic ban: posting a message containing spam words', days: 7}).catch(console.error)
+      client.guilds.filter(g=>g.me.hasPermission('BAN_MEMBERS')).forEach(guild => {
+        guild.ban(message.member.id, 'Spam content/link in messages').catch(console.error)
+      })
+      const spammer = await client.db.forceUser(message.member.id)
+      spammer.powerlevel = -2
+      spammer.blacklistReason = 'Automatic ban: posting a message containing spam words'
+      await client.db.updateUser(spammer)
+
+      logHook.send(`:bomb: \`${message.member.id}\` has been automatically gbanned for posting spam messages`)
+
+      return
+    }
+  }
+
   if(client.config.allowMentionPrefix) message.content = message.content.replace(new RegExp(`^<@!?${client.user.id}> `), client.config.prefix)
 
   if (!message.content.startsWith(client.config.prefix)) return
-  if (message.author.bot) return
 
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase()
