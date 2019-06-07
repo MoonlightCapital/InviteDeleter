bb16d5ed0b5f66257e7880d8e2be351a33b6a399
MoonlightCapital <supertoad65@yahoo.com>
2019-06-07 20:31:50 +0200

    Fix server remove log

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,5 +1,5 @@
 const {logHook} = require('../includes/logging')
 
-module.exports = (client, guild) => {
-  logHook.send(`:arrow_left: Removed from a server: **${client.utils.escapeMarkdown(guild.name)}** (\`${guild.id}\`) owned by ${client.utild.escapeMarkdown(guild.owner.user.tag)} (\`${guild.ownerID}\`)`)
+module.exports = async (client, guild) => {
+  logHook.send(`:arrow_left: Removed from a server: **${client.utils.escapeMarkdown(guild.name)}** (\`${guild.id}\`) owned by ${client.utils.escapeMarkdown(guild.owner.user.tag)} (\`${guild.ownerID}\`)`)
 }
